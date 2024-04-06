534bf82b412c69cbfd604e3d0e736f4dfbb26a4f
Joni Helén <91052459+JoniHelen@users.noreply.github.com>
2024-04-06 05:13:29 +0300

    UI start

--- Assets/Scripts/Island.cs
+++ Assets/Scripts/Island.cs
@@ -1,11 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-
-using UnityEditor;
-using UnityEditor.UIElements;
-using UnityEngine.UIElements;
+using TMPro;
 
 public class Island : MonoBehaviour
 {
@@ -14,9 +10,39 @@ public class Island : MonoBehaviour
     {
         public CollectableItem.ItemType Type;
         public int Count;
+        
+        public string Name => Type switch
+        {
+            CollectableItem.ItemType.Board => "Board",
+            CollectableItem.ItemType.SlidePiece => "Slide Piece",
+            CollectableItem.ItemType.Parasol => "Parasol",
+            CollectableItem.ItemType.Thatch => "Thatch",
+            CollectableItem.ItemType.Nails => "Nails",
+            CollectableItem.ItemType.Hammer => "Hammer",
+            CollectableItem.ItemType.Saw => "Saw",
+            CollectableItem.ItemType.Brimstone => "Brimstone",
+            CollectableItem.ItemType.Sand => "Sand",
+            CollectableItem.ItemType.TreasureChest => "Treasure Chest",
+            CollectableItem.ItemType.Coins => "Coins",
+            CollectableItem.ItemType.Jewels => "Jewels",
+            _ => ""
+        };
     }
     
     public List<ItemInfo> RequiredItems;
+
+    [SerializeField] private TMP_Text text;
+
+
+    private void Awake()
+    {
+        text.text = "Items required:";
+
+        foreach (var item in RequiredItems)
+        {
+            text.text += $" {item.Count}x {item.Name}";
+        }
+    }
 
     public bool CanBuildIsland(List<CollectableItem> items)
     {
